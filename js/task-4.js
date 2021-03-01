487585e521242bc03673b18cf1d04216605ab7b5
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-03-01 20:59:55 +0200

    HW-7 (task-4 updated)

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,19 @@
 // создаем переменные: текущее значение спана, кнопку увелисения и кнопку уменьшения:
-const counterValue = document.querySelector('#value');
+let counterValue = Number(document.querySelector('#value').textContent);
 const increaseBtn = document.querySelector('[data-action="increment"]');
 const decreaseBtn = document.querySelector('[data-action="decrement"]');
-
-// создаем функции increment и decrement для увеличения и уменьшения значения счетчика
-const increase = () => 
-counterValue.textContent = Number(counterValue.textContent) + 1;
-
-const decrease = () => 
-counterValue.textContent = Number(counterValue.textContent) - 1;
 
 // добавляем слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 increaseBtn.addEventListener('click', increase);
 decreaseBtn.addEventListener('click', decrease);
 
+// создаем функции increment и decrement для увеличения и уменьшения значения счетчика
+function increase() {
+    counterValue += 1;
+    document.querySelector('#value').textContent = counterValue;   
+}
 
-
+function decrease() {
+    counterValue -= 1;
+    document.querySelector('#value').textContent = counterValue; 
+}
